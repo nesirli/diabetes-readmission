844d03c2d0c7c2a68030d52d3ed715d1f51a22c2
Nasir Nesirli <nasir.nesirli@gmail.com>
2026-07-17 12:00:15 +0300

    test: add load data test script

---
+++ tests/test_load_data.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+from src.diabetes_readmission.ingest.load_data import load_data
+
+
+def test_load_data():
+    """Test the load_data function."""
+    df = load_data()
+
+    # Check if the returned object is a pandas DataFrame
+    assert isinstance(df, pd.DataFrame), "The returned object is not a pandas DataFrame."
+
+    # Check if the DataFrame has the expected number of rows and columns
+    assert df.shape[0] == 101766
+    assert df.shape[1] == 48
